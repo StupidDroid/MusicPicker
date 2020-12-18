0df1cfb55b4dcb1dcf0606b980199271e0471436
Tucker Quering <tdquering@gmail.com>
2020-12-18 09:32:27 -0500

    The skeleton has been prepared. Next up: get it to actually play some songs.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import csv
 import random
-import math
+import operator
 
 
 def oneToFour(criterion):
@@ -8,7 +8,7 @@ def oneToFour(criterion):
     while(input_value < 1 or input_value > 4):
         try:
             input_value = int(input("1. Low\n2. Medium \n3. High \n"
-                                    "4. Surprise \nPlease input " + criterion +
+                                    "4. Neutral \nPlease input " + criterion +
                                     " (1-4): "))
         except ValueError:
             pass
@@ -24,21 +24,23 @@ def meetsCriterion(criterion, song):
         return (song_value >= 4 and song_value <= 8)
     elif(criterion[1] == 3):
         return (song_value >= 6)
+    elif(criterion[1] == 4):
+        return True
 
 
 def correlation(criteria, active_song, prosp_song):
     correlation = 0
-    correlation += (1.0 * (9 - math.abs(int(active_song[criteria[0][0] + 1]) -
+    correlation += (1.0 * (9 - abs(int(active_song[criteria[0][0] + 1]) -
                     int(prosp_song[criteria[0][0] + 1]))))
-    correlation += (0.9 * (9 - math.abs(int(active_song[criteria[1][0] + 1]) -
+    correlation += (0.9 * (9 - abs(int(active_song[criteria[1][0] + 1]) -
                     int(prosp_song[criteria[1][0] + 1]))))
-    correlation += (0.75 * (9 - math.abs(int(active_song[criteria[2][0] + 1]) -
+    correlation += (0.75 * (9 - abs(int(active_song[criteria[2][0] + 1]) -
                     int(prosp_song[criteria[2][0] + 1]))))
-    correlation += (0.6 * (9 - math.abs(int(active_song[criteria[3][0] + 1]) -
+    correlation += (0.6 * (9 - abs(int(active_song[criteria[3][0] + 1]) -
                     int(prosp_song[criteria[3][0] + 1]))))
-    correlation += (0.5 * (9 - math.abs(int(active_song[criteria[4][0] + 1]) -
+    correlation += (0.5 * (9 - abs(int(active_song[criteria[4][0] + 1]) -
                     int(prosp_song[criteria[4][0] + 1]))))
-    correlation += (0.3 * (9 - math.abs(int(active_song[criteria[5][0] + 1]) -
+    correlation += (0.3 * (9 - abs(int(active_song[criteria[5][0] + 1]) -
                     int(prosp_song[criteria[5][0] + 1]))))
 
     if(active_song[8] == prosp_song[8]):
@@ -48,6 +50,20 @@ def correlation(criteria, active_song, prosp_song):
         correlation += 1
 
     correlation += (random.randint(-10, 10) / 10)
+    correlation = round(correlation, 1)
+    return correlation
+
+
+def songsMatchingCriterion(criterion, pool):
+    matched_songs = []
+    for song in pool:
+        if(meetsCriterion(criterion, song)):
+            matched_songs.append(song)
+
+    if(len(matched_songs) > 0):
+        return matched_songs
+    else:
+        return pool
 
 
 with open('song_db.csv') as csvfile:
@@ -79,11 +95,8 @@ with open('song_db.csv') as csvfile:
     important_index -= 1
 
     sorted_criteria = []
-    if(numerical_inputs[important_index][1] == 4):
-        numerical_inputs[important_index] = (numerical_inputs[important_index]
-                                             [0], random.randint(1, 3))
-
     sorted_criteria.append(numerical_inputs.pop(important_index))
+
     non_surprise_criteria = []
     for i in range(0, len(numerical_inputs)):
         if(numerical_inputs[i][1] != 4):
@@ -100,9 +113,6 @@ with open('song_db.csv') as csvfile:
             surprise_criteria.append(numerical_inputs[i])
 
     for i in range(0, len(surprise_criteria)):
-        surprise_criteria[i] = (surprise_criteria[i][0], random.randint(1, 3))
-
-    for i in range(0, len(surprise_criteria)):
         sorted_criteria.append(surprise_criteria.pop
                                (random.randint(0, len(surprise_criteria) - 1)))
 
@@ -113,7 +123,26 @@ with open('song_db.csv') as csvfile:
            and meetsCriterion(sorted_criteria[1], song)):
             song_pool.append(song)
 
-    current_song = song_pool.pop(random.randint(0, len(song_pool) - 1))
+    ordered_playlist = []
+    possible_first_songs = song_pool
+    for criterion in sorted_criteria:
+        possible_first_songs = songsMatchingCriterion(criterion,
+                                                      possible_first_songs)
+        if(len(possible_first_songs) == 1):
+            break
+    first_song_index = song_pool.index(possible_first_songs[random.randint
+                                       (0, len(possible_first_songs) - 1)])
+    ordered_playlist.append(song_pool.pop(first_song_index))
+
     while(len(song_pool) > 0):
-        print(current_song[0])
-        input("Press enter for next song: ")
+        song_correlations = {}
+        for i in range(0, len(song_pool)):
+            corr = correlation(sorted_criteria,
+                               ordered_playlist[-1], song_pool[i])
+            song_correlations[i] = corr
+        next_song_index = max(song_correlations.items(),
+                              key=operator.itemgetter(1))[0]
+        ordered_playlist.append(song_pool.pop(next_song_index))
+
+    for song in ordered_playlist:
+        print(song[0])
